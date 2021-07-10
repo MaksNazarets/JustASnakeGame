c2d82903a4fdab38ff963a6f061ace9bff2eab58
Maks Nazarets <48256242+MaksNazarets@users.noreply.github.com>
2021-07-10 17:03:08 +0300

    HTML canvas implemented

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const body = document.querySelector("body");
 const game = document.querySelector("#game");
 const field = document.querySelector("#back");
-const square = document.querySelector('#q');
-var square_tail = document.querySelectorAll('.q-tail');
 
-const food = document.querySelector("#food");
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d'); 
+
 const scores = document.querySelector("#scores_number");
 
 const gameOver_block = document.querySelector(".game_over_block");
@@ -12,11 +12,9 @@ const gameOver_text = document.querySelector(".game_over_text");
 const restart_button = gameOver_block.querySelector(".restart_btn");
 const pause_block = document.querySelector(".pause_block");
 
-
-
+const rules_container = document.querySelector(".rules-container");
 const rules_btn = document.querySelector("#rules-btn");
 const ok_btn = document.querySelector(".ok-btn");
-const rules_container = document.querySelector(".rules-container");
 
 const records_btn = document.querySelector('.records-btn');
 const records_container = document.querySelector('.records-container')
@@ -32,27 +30,35 @@ var records_array = [0, 0, 0, 0, 0]; // the data is dummy
 // Settings
 var default_speed = 250;
 var speed;
-var posx, posy;
 
 var scores_number = 0;
-
-
-var foodX, foodY;
 
 var direction = {
     is_movingRight: false,
     is_movingLeft: false,
     is_movingUp: false,
-    is_movingDown: false
+    is_movingDown: false,
+    setDirection: (direct) => {
+        direction.is_movingRight = false;
+        direction.is_movingLeft = false;
+        direction.is_movingUp = false;
+        direction.is_movingDown = false;
+        switch(direct){
+            case 'right':
+                direction.is_movingRight = true;
+                break;
+            case 'left':
+                direction.is_movingLeft = true;
+                break;
+            case 'up':
+                direction.is_movingUp = true;
+                break;
+            case 'down':
+                direction.is_movingDown = true;
+                break;    
+        }
+    }
 }
-
-// ===============================
-var is_movingRight = false
-var is_movingLeft = false
-var is_movingUp = false
-var is_movingDown = false
-// ===============================
-
 
 const ONE_EXIS_SECTION_NUMBER = 20;
 
@@ -62,23 +68,80 @@ const FIELD_SIDE = Math.floor(field.clientWidth / 10) * 10;
     field.style.minHeight = FIELD_SIDE + "px";
     field.style.maxWidth = FIELD_SIDE + "px";
     field.style.maxHeight = FIELD_SIDE + "px";
+
+    canvas.width = FIELD_SIDE;
+    canvas.height = FIELD_SIDE;
+
 // ----------------
 
 const SQUARE_SIDE = FIELD_SIDE / ONE_EXIS_SECTION_NUMBER;
-// ---------------
-    square.style.width = SQUARE_SIDE + "px";
-    square.style.height = SQUARE_SIDE + "px";
-// ----------------
-
-const FOOD_SIDE = SQUARE_SIDE
-// ---------------
-    food.style.width = FOOD_SIDE + "px";
-    food.style.height = FOOD_SIDE + "px";
-// ----------------
 
 const ONE_STEP_SIZE = SQUARE_SIDE;
 
+
 var is_over = false;
+
+class Snake{
+    constructor(){
+        this.startX = SQUARE_SIDE * Math.floor(ONE_EXIS_SECTION_NUMBER / 2);
+        this.startY = SQUARE_SIDE * Math.floor(ONE_EXIS_SECTION_NUMBER / 2);
+
+        this.x = this.startX;
+        this.y = this.startY;
+        this.dx = 0;
+        this.dy = 0;
+        this.side = SQUARE_SIDE;
+        this.color = "#000"
+    }
+
+    update() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.side, this.side)
+    }
+}
+
+var snake_body = [];
+
+class SnakeSegment{
+    constructor(){
+        this.x = prev_posX;
+        this.y = prev_posY;
+        this.side = SQUARE_SIDE;
+        this.color = "#000";
+        snake_body.push(this);
+    }
+
+    draw(){
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.side, this.side)
+    }
+}
+
+class Food{
+    constructor(){
+        this.x = undefined,
+        this.y = undefined,
+        this.side = SQUARE_SIDE,
+        this.color = "#424242"
+    }
+
+    draw(){
+        ctx.fillStyle = "#424242";
+        ctx.fillRect(this.x, this.y, this.side, this.side)
+    }
+}
+
+
+const snake = new Snake;
+const food = new Food;
+
+
+
 
 
 
@@ -90,173 +153,111 @@ var move;
 let prev_posX, prev_posY;
 let actual_posX, actual_posY;
 
-function Move(someFunction){
+function Move(){
     unPause();
 
-    someFunction();
-    let updateSnake = () => {
-        if(is_over) return;
+    savePrevPosition();
+    snake.update();
 
-        if(square_tail.length > 0){
-            for (let i = 0; i < square_tail.length; i++){      
-                actual_posX = getComputedStyle(square_tail[i]).left;
-                actual_posY = getComputedStyle(square_tail[i]).top;
-                
-                SetLocation(square_tail[i], prev_posX, prev_posY);
-
-                prev_posX = actual_posX;
-                prev_posY = actual_posY;
-            }
-        }
-
-        let is_on_body = false; 
-        square_tail.forEach(element => {
-            if(getComputedStyle(element).left == posx + 'px' &&
-            getComputedStyle(element).top == posy + 'px') is_on_body = true;
-        });
-
-        if(is_on_body) gameOver();
-
-    }
-
-    updateSnake();
-
-    move = setInterval(() => {
-        someFunction();
-        updateSnake();
-    }, speed);
-}
-
-function SetDirection(someBoolDirectionVar) {    
-    direction.is_movingRight = false;
-    direction.is_movingLeft = false;
-    direction.is_movingUp = false;
-    direction.is_movingDown = false;
-}
-
-function SetLocation(element, posX, posY) {
-    element.style.left = posX;
-    element.style.top = posY;
-}
-
-function moveRight(){
-    if(direction.is_movingLeft) return;
-
-    stopMoving();
-    Move(MoveR);
-
-    function MoveR(){
-        // SetDirection(direction.is_movingRight);
-        direction.is_movingRight = true;
-        direction.is_movingLeft = false;
-        direction.is_movingUp = false;
-        direction.is_movingDown = false;
-
-        prev_posX = posx + 'px';
-        prev_posY = posy + 'px';
-
-        posx += ONE_STEP_SIZE;
-
-        if(posx >= FIELD_SIDE){ 
-            if(!is_over) gameOver();
+    if(snake.x < 0 ||
+        snake.x == canvas.width ||
+        snake.y < 0 ||
+        snake.y == canvas.height){
+            gameOver();
             return;
+    } 
+
+    CheckIntersect();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    snake.draw();
+    updateSnakeBody();
+    food.draw();
+
+
+    let is_on_body = false; 
+    snake_body.forEach(segment => {
+        segment.draw();
+
+        if(segment.x == snake.x &&
+            segment.y == snake.y) is_on_body = true;
+    });
+    
+    if(is_on_body){
+        gameOver();
+        return;
+    } 
+
+    // console.log('Step');
+
+    move = setTimeout(Move, speed);
+}
+
+function savePrevPosition() {
+    prev_posX = snake.x;
+    prev_posY = snake.y;
+}
+
+function SetSegmentLocation(element, posX, posY) {
+    element.x = posX;
+    element.y = posY;
+}
+
+function updateSnakeBody(){
+    if(snake_body.length){
+        for (let i = 0; i < snake_body.length; i++){      
+            actual_posX = snake_body[i].x;
+            actual_posY = snake_body[i].y;
+          
+            SetSegmentLocation(snake_body[i], prev_posX, prev_posY);
+
+            prev_posX = actual_posX;
+            prev_posY = actual_posY;
         }
-
-        square.style.left = posx + "px";
-        CheckIntersect();
-
     }
 }
 
-function moveLeft(){
-    if(direction.is_movingRight) return;
 
-    stopMoving();
-    Move(MoveL);
-
-
-    function MoveL(){
-        // SetDirection(direction.is_movingLeft);
-        direction.is_movingRight = false;
-        direction.is_movingLeft = true;
-        direction.is_movingUp = false;
-        direction.is_movingDown = false;
-
-        prev_posX = posx + 'px';
-        prev_posY = posy + 'px';
-
-        posx -= ONE_STEP_SIZE;
-        if(posx < 0){
-            if(!is_over) gameOver();
-            return;
-        }
-
-        square.style.left = posx + "px";
-
-        CheckIntersect();
+function moveRight() {
+    if(!direction.is_movingLeft){
+        stopMoving();
+        snake.dx = ONE_STEP_SIZE;
+        Move();
+        direction.setDirection('right');
     }
 }
-
-function moveUp(){
-    if(direction.is_movingDown) return;
-
-    stopMoving();
-    Move(MoveUp);
-
-    function MoveUp(){
-        // SetDirection(direction.is_movingUp);
-        direction.is_movingRight = false;
-        direction.is_movingLeft = false;
-        direction.is_movingUp = true;
-        direction.is_movingDown = false;
-
-        prev_posX = posx + 'px';
-        prev_posY = posy + 'px';
-
-        posy -= ONE_STEP_SIZE;
-        if(posy < 0){ 
-            if(!is_over) gameOver();
-            return;
-        }
-
-        square.style.top = posy + "px";
-
-        CheckIntersect();
-    }
-
-}
-
-function moveDown(){
-    if(direction.is_movingUp) return;
-
-
-    stopMoving();
-    Move(MoveDown);
-
-    function MoveDown(){
-        // SetDirection(direction.is_movingDown);
-        direction.is_movingRight = false;
-        direction.is_movingLeft = false;
-        direction.is_movingUp = false;
-        direction.is_movingDown = true;
-
-        prev_posX = posx + 'px';
-        prev_posY = posy + 'px';
-        
-        posy += ONE_STEP_SIZE;
-        if(posy >= FIELD_SIDE){ 
-            if(!is_over) gameOver();
-            return;
-        }
-
-        square.style.top = posy + "px";
-
-        CheckIntersect();
+function moveLeft() {
+    if(!direction.is_movingRight){
+        stopMoving();
+        snake.dx = -ONE_STEP_SIZE;
+        Move();
+        direction.setDirection('left');
     }
 }
+function moveUp() {
+    if(!direction.is_movingDown){
+        stopMoving();
+        snake.dy = -1 * ONE_STEP_SIZE;
+        Move();
+        direction.setDirection('up');
+    }
+}
+function moveDown() {
+   if(!direction.is_movingUp){
+        stopMoving();
+        snake.dy = ONE_STEP_SIZE;
+        Move();
+        direction.setDirection('down');
+   }
+}
+
+
 
 function stopMoving(){
     clearInterval(move);
+    snake.dx = 0;
+    snake.dy = 0;
 }
 
 function setPause(){
@@ -275,44 +276,69 @@ document.addEventListener('keydown', function(event) {
         else if (event.code == 'ArrowRight' ) moveRight();
         else if (event.code == 'ArrowLeft' ) moveLeft();
         else if (event.code == 'Space') setPause();
+        records_container.classList.add('rec-container_hidden');
+        settings_menu.classList.add('sm-hidden');
+        rules_container.classList.add('rc-hidden');
     }
-    records_container.classList.add('rec-container_hidden');
-    settings_menu.classList.add('sm-hidden');
-    rules_container.classList.add('rc-hidden');
+    
 });
 
-document.addEventListener('keyup', function(event) {
+document.addEventListener('keydown', function(event) {
     if (is_over && event.code == 'Space') restart_button.click();
 });
+
+
+
+rules_btn.addEventListener("click", () => {
+    rules_container.classList.remove("rc-hidden");
+})
+
+ok_btn.addEventListener("click", () => {
+    rules_container.classList.add("rc-hidden");
+})
+
+records_btn.addEventListener("click", () => {
+    records_container.classList.toggle("rec-container_hidden");
+    FillInRecordList();
+})
+
+settings_btn.addEventListener('click', () => {
+    settings_menu.classList.toggle('sm-hidden');
+})
+
+
+settings_menu.addEventListener('click', (event) => {
+    switch(event.target.id){
+        case 'clear-records':
+            records_array = [0, 0, 0, 0, 0];
+            localStorage.setItem('records', JSON.stringify(records_array));
+            FillInRecordList();
+            ShowMessage('Очищено');
+            break;
+    }
+})
 
 
 
 //             = GAME PROCESS =
 
 function spawnFood(){
-    foodX = SQUARE_SIDE * getRandomInt(1, 19);
-    foodY = SQUARE_SIDE * getRandomInt(1, 19);
-
-    let is_on_snake = false; 
-    square_tail.forEach(element => {
-        if(getComputedStyle(element).left == foodX + 'px' &&
-        getComputedStyle(element).top == foodY + 'px') is_on_snake = true;
-        return
-    });
-
+    
+    let is_on_snake = true; 
+    
     while(is_on_snake){
-        foodX = SQUARE_SIDE * getRandomInt(1, 19);
-        foodY = SQUARE_SIDE * getRandomInt(1, 19);
-        is_on_snake = false;
-        square_tail.forEach(element => {
-            if(getComputedStyle(element).left == foodX + 'px'&&
-            getComputedStyle(element).top == foodY + 'px') is_on_snake = true;
-            return;
-        });
+        food.x = SQUARE_SIDE * getRandomInt(1, 19);
+        food.y = SQUARE_SIDE * getRandomInt(1, 19);
+
+        is_on_snake = false
+
+        snake_body.forEach(segment => {
+            if(segment.x == food.x &&
+                segment.y == food.y) is_on_snake = true;
+        })
     }
 
-    food.style.left = foodX + 'px';
-    food.style.top = foodY + 'px';
+    food.draw();
 }
 
 function getRandomInt(min, max) {
@@ -320,26 +346,17 @@ function getRandomInt(min, max) {
 }
 
 function CheckIntersect() {
-    if(posy == foodY && 
-        posx == foodX ){
+    if(snake.x == food.x && 
+        snake.y == food.y ){
         plusScore();
     }
 }
 
 function plusScore(){
-    square_tail = document.querySelectorAll('.q-tail'); // getting actual tail-block array    
+    new SnakeSegment;
 
     scores_number++;
     scores.textContent = scores_number;
-
-    var tail_segment = document.createElement("div");
-    tail_segment.classList.add("q-tail");
-
-    tail_segment.style.width = SQUARE_SIDE + 'px';
-    tail_segment.style.height = SQUARE_SIDE + 'px';
-
-    field.appendChild(tail_segment);
-    square_tail = document.querySelectorAll('.q-tail');
 
     spawnFood();
 
@@ -358,18 +375,33 @@ function plusScore(){
             break;
         case 50:
             speed -= 15;
-            break;    
+            break;
+        case 70:
+            speed -= 15;
+            break;     
     }
 
 }
 
 function gameOver(){
     is_over = true;
-    clearInterval(move);
+    stopMoving();
 
     UpdateRecordList();
 
-    square.classList.add("q-game_over");
+    // let go_animation = setInterval(() => {
+    //     snake.color = "gray";
+    //     Move();
+    //     setTimeout(() => {
+    //         snake.color = "#000";
+    //         Move();
+    //     }, 150);
+    // },300)
+
+    // setTimeout(() => {
+    //     clearInterval(go_animation)
+    // }, 1000)
+
     gameOver_block.classList.add("go-block_visible");
     gameOver_text .classList.add("go-text_visible");
     restart_button.classList.add("go-text_visible");
@@ -379,30 +411,24 @@ restart_button.onclick = newGame;
 
 function newGame(){
     is_over = false;
-    stopMoving();
 
-    square_tail = document.querySelectorAll('.q-tail');
-    if(square_tail.length > 0){
-        square_tail.forEach(element => {
-            element.parentNode.removeChild(element);
-        });
-    }
+    snake_body = [];
+
+    snake.x = snake.startX;
+    snake.y = snake.startY;
+
+    speed = default_speed;
+    Move();
 
     gameOver_block.classList.remove("go-block_visible");
     restart_button.classList.remove("go-btn_visible");
     gameOver_text.classList.remove("go-text_visible");
-    square.classList.remove("q-game_over");
     restart_button.classList.remove("go-text_visible");
 
     scores_number = 0;
     scores.textContent = scores_number;
 
-    posx = Math.floor(ONE_EXIS_SECTION_NUMBER / 2 * SQUARE_SIDE);
-    posy = posx;
     speed = default_speed;
-
-    square.style.left = posx + "px";
-    square.style.top = posy+ "px";
 
     spawnFood();
 };
@@ -435,7 +461,7 @@ function FillInRecordList() {
 }
 
 function ShowMessage(text) {
-    msg.querySelector('.message-text').innerHTML = text;
+    msg.querySelector('.message-text').textContent = text;
 
     msg.classList.remove('message-container-hidden');
 
@@ -444,38 +470,6 @@ function ShowMessage(text) {
     }, 3000);       
 }
 
-
-
-
-rules_btn.addEventListener("click", () => {
-    rules_container.classList.remove("rc-hidden");
-})
-
-ok_btn.addEventListener("click", () => {
-    rules_container.classList.add("rc-hidden");
-})
-
-records_btn.addEventListener("click", () => {
-    records_container.classList.toggle("rec-container_hidden");
-    FillInRecordList();
-})
-
-settings_btn.addEventListener('click', () => {
-    settings_menu.classList.toggle('sm-hidden');
-})
-
-
-
-settings_menu.addEventListener('click', (event) => {
-    switch(event.target.id){
-        case 'clear-records':
-            records_array = [0, 0, 0, 0, 0];
-            localStorage.setItem('records', JSON.stringify(records_array));
-            FillInRecordList();
-            ShowMessage('Очищено');
-            break;
-    }
-})
 
 
 // =========== Mobile controls ==========
@@ -491,7 +485,7 @@ mobile_controls.addEventListener('click', (event) => {
                 moveRight();
                 break;
             case 'arr-up':
-                moveUp();
+                moveUp(); 
                 break;
             case 'arr-down':
                 moveDown();
@@ -512,9 +506,5 @@ mobile_controls.addEventListener('click', (event) => {
 
 //        ======= GAME SCRIPT =======
 
-    newGame();
-    FillInRecordList()
-
-
-   
-
+newGame();
+FillInRecordList()
